bff4ea25b098405b80cce5d48d65fe572c492a4c
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-15 23:37:45 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login'
 import {Register} from './features/auth/register/register';
 import {Dashboard} from './features/dashboard/dashboard/dashboard';
+import {authGuard} from './core/Guards/Auth-Guard';
 
 export const routes: Routes = [
   {path: 'login', component: Login},
   {path: 'register', component: Register},
-  {path: 'dashboard', component: Dashboard},
+  {path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
