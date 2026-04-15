e684ea158bc94c9844af5510a7604fdf822cc3a5
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-15 19:58:51 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login'
 import {Register} from './features/auth/register/register';
+import {Dashboard} from './features/dashboard/dashboard/dashboard';
 
 export const routes: Routes = [
   {path: 'login', component: Login},
   {path: 'register', component: Register},
+  {path: 'dashboard', component: Dashboard},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-
 ];
--- src/app/features/dashboard/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Navbar } from '../../layout/navbar/navbar';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Navbar],
+  imports: [Navbar, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush
